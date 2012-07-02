ea81a9341dea28b2b6a77a91da0874fedfaa26e9
Cosmin Radoi <cosmin.radoi@gmail.com>
2012-07-02 18:45:44 -0500

    common changes

---
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/concurrency/ConcurrencyRefactorings.java
@@ -0,0 +1,34 @@
+package org.eclipse.jdt.internal.corext.refactoring.concurrency;
+
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * Helper class to get NLSed messages.
+ */
+public final class ConcurrencyRefactorings extends NLS {
+
+	private static final String BUNDLE_NAME= ConcurrencyRefactorings.class.getName();
+
+	private ConcurrencyRefactorings() {
+		// Do not instantiate
+	}
+	
+	public static String ConcurrencyRefactorings_update_imports;
+	public static String ConcurrencyRefactorings_type_error;
+	public static String ConcurrencyRefactorings_empty_string;
+	public static String ConcurrencyRefactorings_program_name;
+	public static String ConcurrencyRefactorings_field_compile_error;
+	public static String ConcurrencyRefactorings_read_access;
+	public static String ConcurrencyRefactorings_write_access;
+	public static String ConcurrencyRefactorings_postfix_access;
+	public static String ConcurrencyRefactorings_prefix_access;
+	public static String ConcurrencyRefactorings_remove_synch_mod;
+	public static String ConcurrencyRefactorings_remove_synch_block;
+	public static String ConcurrencyRefactorings_unsafe_op_error_1;
+	public static String ConcurrencyRefactorings_unsafe_op_error_2;
+	public static String ConcurrencyRefactorings_unsafe_op_error_3;
+	
+	static {
+		NLS.initializeMessages(BUNDLE_NAME, ConcurrencyRefactorings.class);
+	}
+}

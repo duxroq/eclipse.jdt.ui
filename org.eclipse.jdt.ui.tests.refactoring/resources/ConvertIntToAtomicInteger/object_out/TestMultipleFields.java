e4ae1668a67f2d76faf60f016a29c295aec2db5a
duxroq <alexandria.shearer@gmail.com>
2012-07-05 21:28:49 -0300

    Fixed an object_out resource file (added semi-colon)

--- org.eclipse.jdt.ui.tests.refactoring/resources/ConvertIntToAtomicInteger/object_out/TestMultipleFields.java
+++ org.eclipse.jdt.ui.tests.refactoring/resources/ConvertIntToAtomicInteger/object_out/TestMultipleFields.java
@@ -5,7 +5,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class TestMultipleFields {
 
 	AtomicInteger f = new AtomicInteger();
-	AtomicInteger g = new AtomicInteger()
+	AtomicInteger g = new AtomicInteger();
 
 	void inc() {
 		f.getAndIncrement();

199645cbaf5d6511d7d4b63c3e6e2c498c603063
duxroq <alexandria.shearer@gmail.com>
2012-07-10 12:15:29 -0300

    New ASTVisitor classes to support extended functionality

---
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/concurrency/SideEffectsFinderAtomicInteger.java
@@ -0,0 +1,134 @@
+package org.eclipse.jdt.internal.corext.refactoring.concurrency;
+
+import java.util.List;
+
+import org.eclipse.text.edits.TextEditGroup;
+
+import org.eclipse.ltk.core.refactoring.RefactoringStatus;
+import org.eclipse.ltk.core.refactoring.RefactoringStatusEntry;
+
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.Assignment;
+import org.eclipse.jdt.core.dom.Expression;
+import org.eclipse.jdt.core.dom.FieldAccess;
+import org.eclipse.jdt.core.dom.IBinding;
+import org.eclipse.jdt.core.dom.IVariableBinding;
+import org.eclipse.jdt.core.dom.InfixExpression;
+import org.eclipse.jdt.core.dom.MethodInvocation;
+import org.eclipse.jdt.core.dom.ParenthesizedExpression;
+import org.eclipse.jdt.core.dom.QualifiedName;
+import org.eclipse.jdt.core.dom.SimpleName;
+import org.eclipse.jdt.core.dom.Statement;
+import org.eclipse.jdt.core.dom.SuperFieldAccess;
+import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
+
+public class SideEffectsFinderAtomicInteger extends ASTVisitor {
+
+	private final IVariableBinding notIncludingField;
+	private final RefactoringStatus status;
+	private final Statement enclosingStatement;
+	private final ASTRewrite rewriter;
+	private final List<TextEditGroup> groupDescriptions;
+	private RefactoringStatusEntry statusEntry;
+	private boolean foundFieldInAnInfix= false;
+
+	private static final String READ_ACCESS= "Read Access"; //$NON-NLS-1$
+
+	public SideEffectsFinderAtomicInteger(
+			Statement enclosingStatement, IVariableBinding notIncludingField, RefactoringStatus status, ASTRewrite rewriter, List<TextEditGroup> groupDescriptions) {
+		this.enclosingStatement= enclosingStatement;
+		this.notIncludingField= notIncludingField;
+		this.status= status;
+		this.rewriter= rewriter;
+		this.groupDescriptions= groupDescriptions;
+	}
+
+	@Override
+	public boolean visit(Assignment assignment){
+		// TODO find side effects
+		Expression rightHandSide= assignment.getRightHandSide();
+		if (rightHandSide instanceof InfixExpression) {
+			assignment.getRightHandSide().accept(this);
+			if (foundFieldInAnInfix) {
+				// refactor into an addAndGet() possibly
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public boolean visit(InfixExpression infixExpression) {
+		Expression leftOperand= infixExpression.getLeftOperand();
+		Expression rightOperand= infixExpression.getRightOperand();
+
+		if (!(leftOperand instanceof SimpleName)) {
+			leftOperand.accept(this);
+		} else {
+			// TODO what to do here...
+			if (considerBinding(resolveBinding(leftOperand))) {
+				// change i to i.get()? only if appropriate
+				foundFieldInAnInfix= true;
+			}
+		}
+		if (!(rightOperand instanceof SimpleName)) {
+			rightOperand.accept(this);
+		} else {
+			// TODO what to do here...
+			if (considerBinding(resolveBinding(rightOperand))) {
+				// change i to i.get()? only if appropriate
+				foundFieldInAnInfix= true;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public boolean visit(ParenthesizedExpression parenthesizedExpression) {
+		Expression expression= parenthesizedExpression.getExpression();
+		expression.accept(this);
+		return true;
+	}
+
+	@Override
+	public boolean visit(SimpleName simpleName) {
+		Expression invocation= null;
+
+		if ((!simpleName.isDeclaration()) && (considerBinding(resolveBinding(simpleName)))) {
+			invocation= (MethodInvocation) rewriter.createStringPlaceholder(
+					notIncludingField.getName() + ".get()", ASTNode.METHOD_INVOCATION); //$NON-NLS-1$
+			rewriter.replace(simpleName, invocation, createGroupDescription(READ_ACCESS));
+		}
+		return true;
+	}
+
+	private IBinding resolveBinding(Expression expression) {
+
+		if (expression instanceof SimpleName) {
+			return ((SimpleName) expression).resolveBinding();
+		} else if (expression instanceof QualifiedName) {
+			return ((QualifiedName) expression).resolveBinding();
+		} else if (expression instanceof FieldAccess) {
+			return ((FieldAccess) expression).getName().resolveBinding();
+		} else if (expression instanceof SuperFieldAccess) {
+			return ((SuperFieldAccess) expression).getName().resolveBinding();
+		}
+		return null;
+	}
+
+	private boolean considerBinding(IBinding binding) {
+
+		if (!(binding instanceof IVariableBinding)) {
+			return false;
+		}
+		return notIncludingField.isEqualTo(((IVariableBinding) binding).getVariableDeclaration());
+	}
+
+	private TextEditGroup createGroupDescription(String name) {
+
+		TextEditGroup result= new TextEditGroup(name);
+
+		groupDescriptions.add(result);
+		return result;
+	}
+}
